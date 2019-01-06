2fcec6733271138ade0c1c9bc78b5d48ac39307c
luchikm86 <luchik.m86@gmail.com>
2019-01-06 14:34:44 +0200

    add 0723

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0722/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task07.task0722;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Это конец
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = 0; true; i++){
+            list.add(reader.readLine());
+            if (list.get(i).equals("end")) {
+                list.remove(i);
+                break;
+            }
+        }
+
+        for (String s: list) {
+            System.out.println(s);
+        }
+    }
+}
