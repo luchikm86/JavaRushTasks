397b15076d1f38d39c9eb3941bc42f9fc898646a
luchikm86 <luchik.m86@gmail.com>
2019-01-12 23:25:29 +0200

    add 0813

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0813/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task08.task0813;
+
+import java.util.Set;
+import java.util.HashSet;
+
+/* 
+20 слов на букву «Л»
+*/
+
+public class Solution {
+    public static Set<String> createSet() {
+        Set<String> set = new HashSet<>();
+        set.add("Лученко");
+        set.add("Лубны");
+        set.add("Любить");
+        set.add("Львов");
+        set.add("Лошадь");
+
+        set.add("Людмила");
+        set.add("Ларец");
+        set.add("Лошара");
+        set.add("Леший");
+        set.add("Лучший");
+
+        set.add("Лилипут");
+        set.add("Лисапед");
+        set.add("Ламборгини");
+        set.add("Лисабон");
+        set.add("Люксембург");
+
+        set.add("Лондон");
+        set.add("Лада");
+        set.add("Лагуна");
+        set.add("Лодка");
+        set.add("Лаз");
+
+        return set;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
