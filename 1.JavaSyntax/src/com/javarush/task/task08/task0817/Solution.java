f1fccb6a2f6f7e77ca0c275458ba0b22ecc1e29f
luchikm86 <luchik.m86@gmail.com>
2019-04-04 12:15:04 +0300

    add 0817

--- 1.JavaSyntax/src/com/javarush/task/task08/task0817/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0817/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task08.task0817;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,51 +11,46 @@ import java.util.Map;
 public class Solution {
     public static HashMap<String, String> createMap() {
         HashMap<String, String> map = new HashMap<>();
-        map.put("Sidorov", "Miha");
-        map.put("Ivanov", "Andrey");
-        map.put("Stalone", "Silvestr");
-        map.put("Pupkin", "Vasya");
-        map.put("Shtirlic", "Aleksandr");
-        map.put("Solomkin", "Petr");
-        map.put("Bosoy", "Nikolai");
-        map.put("Chonkin", "Ivan");
-        map.put("Zhul", "Vern");
-        map.put("Kusto", "Zhak");
+        map.put("1", "1");
+        map.put("2", "2");
+        map.put("3", "1");
+        map.put("4", "2");
+        map.put("5", "3");
+        map.put("6", "4");
+        map.put("7", "4");
+        map.put("8", "5");
+        map.put("9", "1");
+        map.put("10", "1");
         return map;
+
     }
 
     public static void removeTheFirstNameDuplicates(Map<String, String> map) {
-//        for (Map.Entry<String, String> pair : map.entrySet()) {
-//            String name = pair.getValue();
-//            removeItemFromMapByValue(map, name);
-//        }
+        HashMap<String, String> copy = new HashMap<>(map);
+        for (Map.Entry<String, String> pair : copy.entrySet()) {
+            int count = Collections.frequency(copy.values(), pair.getValue());
+            if (count > 1)
+                removeItemFromMapByValue(map, pair.getValue());
+
+        }
+
 
     }
 
     public static void removeItemFromMapByValue(Map<String, String> map, String value) {
-        HashMap<String, String> copy = new HashMap<String, String>(map);
-        for (Map.Entry<String, String> pair : copy.entrySet()) {
-            if (pair.getValue().equals(value))
-                map.remove(pair.getKey());
+        HashMap<String, String> copy = new HashMap<>(map);
+        for (Map.Entry<String, String> pair : copy.entrySet()) {          ////iterator
+            int m = Collections.frequency(copy.values(), pair.getValue());////count frequency of all value
+            if (m > 1)
+                removeItemFromMapByValue(map, pair.getValue());                        ////remove key with duplicate value
+
         }
     }
 
     public static void main(String[] args) {
 
-//        HashMap<String, String> map = new HashMap<>();
-//        map.put("Sidorov", "Miha");
-//        map.put("Ivanov", "Andrey");
-//        map.put("Stalone", "Silvestr");
-//        map.put("Pupkin", "Vasya");
-//        map.put("Shtirlic", "Aleksandr");
-//        map.put("Solomkin", "Petr");
-//        map.put("Bosoy", "Nikolai");
-//        map.put("Chonkin", "Ivan");
-//        map.put("Zhul", "Vern");
-//        map.put("Kusto", "Zhak");
-//        System.out.println(map);
-//        removeTheFirstNameDuplicates(map);
-//
+        removeTheFirstNameDuplicates(createMap());
+
 
 
     }
